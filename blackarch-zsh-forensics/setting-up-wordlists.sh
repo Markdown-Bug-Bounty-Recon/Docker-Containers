65ad6919eea1e08a60a61006f90a654b39ccb179
Cloufish <cloufish@protonmail.com>
2021-09-04 17:12:25 +0200

    Update

--- blackarch-zsh-forensics/setting-up-wordlists.sh
+++ blackarch-zsh-forensics/setting-up-wordlists.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # Setting up wordlists
+mkdir /home/penelope/SecLists
+cd /home/penelope/SecLists || { echo "Failure in cd command"; exit 1; }
 
-mkdir -p /home/penelope/wordlists
-cd /home/penelope/wordlists || { echo "Failure in cd command"; exit 1; }
+mkdir Passwords
+cd Passwords  || { echo "Failure in cd command"; exit 1; }
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-1000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-10000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-100000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords-1000000.txt
 
+cd ..
+
+wget https://gitlab.com/kalilinux/packages/wordlists/-/raw/kali/master/rockyou.txt.gz
+gunzip rockyou.txt.gz
+
+mkdir Usernames
+cd Usernames  || { echo "Failure in cd command"; exit 1; }
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/xato-net-10-million-usernames-dup.txt -O ./xato-net-10-million-usernames-medium.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/xato-net-10-million-usernames.txt
+cd .
--- blackarch-zsh-minimal/install.sh
+++ blackarch-zsh-minimal/install.sh
@@ -7,9 +7,15 @@ mkdir /home/penelope/tools
 cd /home/penelope/tools || { echo "Failure in cd command"; exit 1; }
 
 # yay
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git || { echo "Failure in cd command"; exit 1; }
-makepkg --noconfirm --needed -si
+#git clone https://aur.archlinux.org/yay-git.git
+#cd yay-git || { echo "Failure in cd command"; exit 1; }
+#makepkg --noconfirm --needed -si
+
+
+
+git clone https://aur.archlinux.org/yay.git
+cd yay || { echo "Failure in cd command"; exit 1; }
+makepkg -si --noconfirm --needed
 cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
 
 rm -rf /home/penelope/tools
