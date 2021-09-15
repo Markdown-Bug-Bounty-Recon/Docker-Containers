d7539d1bf86c4b1a8497bd83a6d03eee13074214
Cloufish <cloufish@protonmail.com>
2021-09-15 18:18:49 +0200

    Add arsenal and ciphey

--- blackarch-zsh-minimal/install.sh
+++ blackarch-zsh-minimal/install.sh
@@ -9,5 +9,18 @@ cd /home/penelope/tools || { echo "Failure in cd command"; exit 1; }
 # yay
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git || { echo "Failure in cd command"; exit 1; }
-makepkg --noconfirm --needed -si
+makepkg --noconfirm --needed -si 
 cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
+
+# arsenal
+git clone https://github.com/Orange-Cyberdefense/arsenal.gi
+cd arsenal || { echo "Failure in cd command"; exit 1; }
+python3 -m pip install -r requirements.txt
+cp ./run /home/penelope/PATH
+cd /home/penelope/tools/ || { echo "Failure in cd command"; exit 1; }
+
+# ciphey
+python3 -m pip install ciphey --upgrade
+
+# Granting permissions
+chmod -R 755 /home/penelope/PATH
